e4ec7c4b82d715b68fb6cf3d439be3768d4cf337
Daniel Romero Duarte <danred001@gmail.com>
2022-06-06 21:01:38 -0600

    subiendo

--- app/src/main/java/com/example/proyectopdm/MyProfile.java
+++ app/src/main/java/com/example/proyectopdm/MyProfile.java
@@ -1,14 +1,128 @@
 package com.example.proyectopdm;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.OnProgressListener;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
 public class MyProfile extends AppCompatActivity {
+
+    EditText editText;
+    Button btn;
+    StorageReference storageReference;
+    DatabaseReference databaseReference;
+
+    ImageView imgGallery;
+    Button btnGallery;
+    private  final int GALLERY_REQ_CODE = 1000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_profile);
+
+        editText=findViewById(R.id.editFile);
+        btn=findViewById(R.id.btnFile);
+        storageReference = FirebaseStorage.getInstance().getReference();
+        databaseReference = FirebaseDatabase.getInstance().getReference("proyectoPDM");
+
+        btn.setEnabled(false);
+        editText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                selectPDF();
+            }
+        });
+
+        imgGallery=(ImageView) findViewById(R.id.imgPerfil);
+        btnGallery=(Button) findViewById(R.id.btnGallery);
+        btnGallery.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent iGallery = new Intent(Intent.ACTION_PICK);
+                iGallery.setData(MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(iGallery, GALLERY_REQ_CODE);
+            }
+        });
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        //for connection
+        if(requestCode == 12 && resultCode == RESULT_OK && data != null && data.getData()!=null){
+            btn.setEnabled(true);
+            editText.setText(data.getDataString().substring(data.getDataString().lastIndexOf("/")+1));
+
+            btn.setOnClickListener(new View.OnClickListener(){
+                @Override
+                public void onClick(View view){
+                    uploadPDFFileFirebase(data.getData());
+                }
+            });
+        }
+
+        //for image
+        if(resultCode == RESULT_OK){
+            if(requestCode == GALLERY_REQ_CODE){
+                imgGallery.setImageURI(data.getData());
+            }
+        }
+    }
+
+    private void uploadPDFFileFirebase(Uri data){
+        final ProgressDialog progressDialog = new ProgressDialog(this);
+        progressDialog.setTitle("File is loading...");
+        progressDialog.show();
+        StorageReference reference = storageReference.child("uploadPDF"+System.currentTimeMillis()+".pdf");
+
+        reference.putFile(data).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>(){
+            @Override
+            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot){
+
+                Task<Uri> uriTask=taskSnapshot.getStorage().getDownloadUrl();
+                while(!uriTask.isComplete());
+                Uri uri=uriTask.getResult();
+                PutPDF putPDF = new PutPDF(editText.getText().toString(),uri.toString());
+                databaseReference.child(databaseReference.push().getKey()).setValue(putPDF);
+                Toast.makeText(MyProfile.this, "File Upload", Toast.LENGTH_LONG).show();
+                progressDialog.dismiss();
+            }
+        }).addOnProgressListener(new OnProgressListener<UploadTask.TaskSnapshot>(){
+            @Override
+            public void onProgress(@NonNull UploadTask.TaskSnapshot snapshot){
+
+                double progress = (100.0 * snapshot.getBytesTransferred())/snapshot.getTotalByteCount();
+                progressDialog.setMessage("File uploaded..."+(int) progress + "%");
+
+            }
+        });
+    }
+
+    private void selectPDF(){
+        Intent intent=new Intent();
+        intent.setType("application/pdf");
+        intent.setAction(intent.ACTION_GET_CONTENT);
+        startActivityForResult(Intent.createChooser(intent, "PDF FILE SELECT"), 12);
+    }
+
 }
--- app/src/main/java/com/example/proyectopdm/PutPDF.java
+++ app/src/main/java/com/example/proyectopdm/PutPDF.java
@@ -1,2 +1,21 @@
-package com.example.proyectopdm;public class PutPDF {
+package com.example.proyectopdm;
+
+public class PutPDF {
+
+    public String name;
+    public String url;
+    public PutPDF(){
+    }
+
+    public PutPDF(String name,String url){
+        this.name=name;
+        this.url=url;
+    }
+
+    public String getName(){
+        return name;
+    }
+    public void setName(String name){
+        this.name=name;
+    }
 }
